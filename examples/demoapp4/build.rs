cd9a3ddae352220675b63e43b5f351a0d1a34be2
Fahim Ahmed <fahimaloy@gmail.com>
2025-09-04 11:31:06 +0600

    chore: ignore plan and guide files; remove temp demo apps; feat(renderer): add EventRuntime with click/dblclick/hover mapping + tests

--- examples/demoapp4/build.rs
+++
@@ -1,4 +0,0 @@
-fn main() {
-    let input = std::path::Path::new(env!("CARGO_MANIFEST_DIR")).join("src/App.vx");
-    velox_cli::build_cmd(&input, Some(&std::path::Path::new(&std::env::var("OUT_DIR").unwrap())), velox_cli::EmitMode::Render).expect("compile App.vx");
-}
